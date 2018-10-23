bd171d2067b4f2ad801c4d7ff9b0dfb2224e6eac
cristianbotina89 <cristian_botina@hotmail.com>
2018-10-22 22:09:12 -0500

    segundo commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,6 +33,7 @@
                     </a>
                 </div>
 
+
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
