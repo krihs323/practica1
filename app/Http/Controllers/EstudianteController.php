bedee46f3fef42fde4a56fbe4472e43d52ca17fe
krihs323 <krihs323@gmail.com>
2018-10-24 23:05:17 -0500

    retorna la llamada del formulario

--- app/Http/Controllers/EstudianteController.php
+++ app/Http/Controllers/EstudianteController.php
@@ -3,10 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Session;
 
 class EstudianteController extends Controller
 {
     public function comprobarNombre(){
       return view('estudiante.formulario');
+    }
+    
+    public function validarNombre(Request $request){
+      //Session::flash('message-error','El deudor es menor de edad');
+      $nombre_apellido = $request["nombre_apellido"];
+      Session::flash('message','El nonbre es '.$nombre_apellido);
+      return redirect()->to('comprobarnombre');
     }
 }
