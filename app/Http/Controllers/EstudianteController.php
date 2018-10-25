0338033a13356f7ddf4f056ef54d3763592561a3
krihs323 <krihs323@gmail.com>
2018-10-24 23:25:37 -0500

    validar nombre de la practica 1

--- app/Http/Controllers/EstudianteController.php
+++ app/Http/Controllers/EstudianteController.php
@@ -14,6 +14,7 @@ class EstudianteController extends Controller
     public function validarNombre(Request $request){
       //Session::flash('message-error','El deudor es menor de edad');
       $nombre_apellido = $request["nombre_apellido"];
+      
       Session::flash('message','El nonbre es '.$nombre_apellido);
       return redirect()->to('comprobarnombre');
     }
